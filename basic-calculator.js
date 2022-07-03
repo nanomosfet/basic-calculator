400489178179534d05762f4c3ee99697a10d9576
Timothy Z Searcy <timmyzsearcy@gmail.com>
2022-07-03 04:47:27 -0700

    init

---
+++ basic-calculator.js
@@ -0,0 +1,25 @@
+// should add a and b
+export const add = (a, b) => {
+    return a + b
+}
+
+  // should subtract a and b
+export const subtract = (a, b) => {
+    // your code goes here
+}
+
+// should divide a and b
+export const divide = (a, b) => {
+    // your code goes here
+}
+
+// should multiply a and b
+export const multiply = (a, b) => {
+    // your code goes here
+}
+
+// should double the input
+export const double = (a) => {
+    // your code goes here
+}
+  
---
+++ test.js
@@ -0,0 +1,57 @@
+import {add, subtract, multiply, divide, double} from './basic-calculator.js'
+import assert from 'assert'
+
+let a = false
+let b = false
+let c = false
+let d = false
+let e = false
+
+try {
+    assert(add(1,1) === 2)
+    console.log('add success')
+    a = true
+} catch {
+    console.log('add failed')
+}
+
+try {
+    assert(subtract(1,1) === 0)
+    console.log('subtract success')
+    b = true
+} catch {
+    console.log('subtract failed 1 - 1 = 0')
+}
+
+try {
+    assert(multiply(10,10) === 100)
+    console.log('multiply success')
+    c = true
+} catch {
+    console.log('mulitply failed example multiply(10, 10) = 100')
+}
+
+try {
+    assert(divide(10,5) === 2)
+    console.log('divide success')
+    d = true
+} catch {
+    console.log('divide failed example divide(10, 2) = 2')
+}
+
+try {
+    assert(double(5) === 10)
+    console.log('double success')
+    e = true
+} catch {
+    console.log('double failed example double(5) = 10')
+}
+
+
+
+
+function _0x2297(_0x46d8bb,_0x6906a7){const _0x30fb33=_0x30fb();return _0x2297=function(_0x2297ea,_0x1cb4f1){_0x2297ea=_0x2297ea-0x14c;let _0x2a048b=_0x30fb33[_0x2297ea];return _0x2a048b;},_0x2297(_0x46d8bb,_0x6906a7);}const _0x417294=_0x2297;(function(_0x10123a,_0x2b542d){const _0x401e58=_0x2297,_0x103f65=_0x10123a();while(!![]){try{const _0x1083d0=parseInt(_0x401e58(0x14c))/0x1*(-parseInt(_0x401e58(0x14e))/0x2)+parseInt(_0x401e58(0x14d))/0x3*(parseInt(_0x401e58(0x152))/0x4)+-parseInt(_0x401e58(0x14f))/0x5+parseInt(_0x401e58(0x151))/0x6+-parseInt(_0x401e58(0x156))/0x7+-parseInt(_0x401e58(0x154))/0x8+parseInt(_0x401e58(0x153))/0x9*(parseInt(_0x401e58(0x157))/0xa);if(_0x1083d0===_0x2b542d)break;else _0x103f65['push'](_0x103f65['shift']());}catch(_0x198a57){_0x103f65['push'](_0x103f65['shift']());}}}(_0x30fb,0xa6f9f));const printSecret=()=>console[_0x417294(0x150)](_0x417294(0x155));function _0x30fb(){const _0x582cf1=['14EXIOCR','21069YwmYwf','54154DgpDFL','696255Oilqod','log','7162344mwqyby','84StEtwl','19425645FlOTqb','9363904hmRjNM','\x0a░░░░░░░░░▄░░░░░░░░░░░░░░▄░░░░\x0a░░░░░░░░▌▒█░░░░░░░░░░░▄▀▒▌░░░\x0a░░░░░░░░▌▒▒█░░░░░░░░▄▀▒▒▒▐░░░\x0a░░░░░░░▐▄▀▒▒▀▀▀▀▄▄▄▀▒▒▒▒▒▐░░░\x0a░░░░░▄▄▀▒░▒▒▒▒▒▒▒▒▒█▒▒▄█▒▐░░░\x0a░░░▄▀▒▒▒░░░▒▒▒░░░▒▒▒▀██▀▒▌░░░\x20\x0a░░▐▒▒▒▄▄▒▒▒▒░░░▒▒▒▒▒▒▒▀▄▒▒▌░░\x0a░░▌░░▌█▀▒▒▒▒▒▄▀█▄▒▒▒▒▒▒▒█▒▐░░\x0a░▐░░░▒▒▒▒▒▒▒▒▌██▀▒▒░░░▒▒▒▀▄▌░\x0a░▌░▒▄██▄▒▒▒▒▒▒▒▒▒░░░░░░▒▒▒▒▌░\x0a▀▒▀▐▄█▄█▌▄░▀▒▒░░░░░░░░░░▒▒▒▐░\x0a▐▒▒▐▀▐▀▒░▄▄▒▄▒▒▒▒▒▒░▒░▒░▒▒▒▒▌\x0a▐▒▒▒▀▀▄▄▒▒▒▄▒▒▒▒▒▒▒▒░▒░▒░▒▒▐░\x0a░▌▒▒▒▒▒▒▀▀▀▒▒▒▒▒▒░▒░▒░▒░▒▒▒▌░\x0a░▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▒▄▒▒▐░░\x0a░░▀▄▒▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▄▒▒▒▒▌░░\x0a░░░░▀▄▒▒▒▒▒▒▒▒▒▒▄▄▄▀▒▒▒▒▄▀░░░\x0a░░░░░░▀▄▄▄▄▄▄▀▀▀▒▒▒▒▒▄▄▀░░░░░\x0a░░░░░░░░░▒▒▒▒▒▒▒▒▒▒▀▀░░░░░░░░\x0a','7888020UvWiUb','10NoIDoQ'];_0x30fb=function(){return _0x582cf1;};return _0x30fb();}
+if (a && b && c && d && e) {
+    printSecret()
+}
+
